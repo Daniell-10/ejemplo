d56eb683f09ec37abb5c9c0d4788c39a72b6a29c
daniell-10 <iva.dan.lon.pin@gmail.com>
2025-03-13 01:20:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    setInput((prev) => prev + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const handleCalculate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch {
+      setInput("Error");
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{input}</div>
+      <div className="buttons">
+        {[
+          "7",
+          "8",
+          "9",
+          "/",
+          "4",
+          "5",
+          "6",
+          "*",
+          "1",
+          "2",
+          "3",
+          "-",
+          "0",
+          ".",
+          "=",
+          "++",
+        ].map((char) => (
+          <button
+            key={char}
+            onClick={() =>
+              char === "=" ? handleCalculate() : handleClick(char)
+            }
+          >
+            {char}
+          </button>
+        ))}
+        <button onClick={handleClear} className="clear">
+          C
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
